c3f721bdc0ee5203365bafac9ee02729626c0acd
AndrewQuezada <andrewevanq11@gmail.com>
2025-10-21 17:03:26 -0700

    question2.js

--- question2.js
+++ question2.js
@@ -1,4 +1,6 @@
+//Discount Eligibility
 const prompt = require("prompt-sync")();
 let num = parseInt (prompt("Enter how much you spent "));
 let result = (num >= 100 ? "10%": "0%");
+
 console.log("You get a "+result+" discount");
