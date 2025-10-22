b18ad34b8c5201d0eadb87d3e0731ebfbe39ccb4
AndrewQuezada <andrewevanq11@gmail.com>
2025-10-21 17:04:22 -0700

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,9 +1,5 @@
+//Weekend Check
 const prompt = require("prompt-sync")();
 let num;
-
 num = prompt("Enter the day of the week ");
-
-
 num =="sunday" || num == "Saturday" ? console.log("It is a weekend") : console.log("It is a Weekday");
-
-
