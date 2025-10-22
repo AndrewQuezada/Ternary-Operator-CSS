4f31a693cd8e64338ca5c8f1543702076f42375c
AndrewQuezada <andrewevanq11@gmail.com>
2025-10-21 17:03:53 -0700

    question3.js

--- question3.js
+++ question3.js
@@ -1,4 +1,6 @@
+//Ticket Price
 const prompt = require("prompt-sync")();
 let num = parseInt (prompt("Enter your age "));
 let result = (num < 12 ? "$8":"$15");
+
 console.log("Your price is "+result);
