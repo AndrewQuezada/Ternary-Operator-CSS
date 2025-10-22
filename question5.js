23e155b763bb12e64b343413f209dd266263844d
AndrewQuezada <andrewevanq11@gmail.com>
2025-10-21 17:05:38 -0700

    question5.js

--- question5.js
+++ question5.js
@@ -1,8 +1,7 @@
+//Age Category
 const prompt = require("prompt-sync")();
 let age;
-
 age = parseInt(prompt("Enter Your age "));
-
 age < 13 ? console.log("you are a kid") :
             age < 20 ?  console.log("you are a teen") :
             age < 60 ? console.log("you are a adult") :
