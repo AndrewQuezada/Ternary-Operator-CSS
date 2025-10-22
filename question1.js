b435ef75e5ebce31c5b28a2fc8398799d27a7267
AndrewQuezada <andrewevanq11@gmail.com>
2025-10-21 17:02:59 -0700

    question1.js

--- question1.js
+++ question1.js
@@ -1,4 +1,6 @@
+//Temperature Status
 const prompt = require("prompt-sync")();
 let num = parseInt (prompt("Enter temperature "));
 let result = (num > 10 ? "Normal": "hot");
+
 console.log("The temprtature is "+result);
